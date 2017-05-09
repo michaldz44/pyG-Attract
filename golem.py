0596c0bde126d2aace0aa4852e238a5af5bdac60
michal <michall44@wp.pl>
2017-05-09 19:58:50 +0200

    end conditions added

--- golem.py
+++ golem.py
@@ -14,7 +14,7 @@ class Golem(object):
         dt=self.velocity*dt+(4.0*self.acceleration0-self.acceleration1)*dt**2
         self.position+=dt
         self.vpredict = self.velocity+ (3.0*self.acceleration0 - self.acceleration1)*dt/2.0
-        self.acceleration2 += self.attractors.get_force(x,y)
+        self.acceleration2 += self.attractors.get_force(self.position)
 
     def do_move(self):
         if self.end():
@@ -23,4 +23,15 @@ class Golem(object):
             return None
 
     def end(self):
+        if self.attractors.min_distance(self.position) < self.args.pot_d and abs(self.velocity) < self.args.term_v: # close to the city and low velocity
+            return True
         
+        
+        
+        
+         if(((min < magnetSize)&&(sqrt(vx*vx+vy*vy)<abortVel))) // blisko i mala predkosc
+							run = 0;
+					   if((sqrt(vx*vx+vy*vy)<minimalVel)&&(ct > minSteps)) // mala predkosc, wykonano duzo krokow, ale dalej niz magnet size
+							run = 0;
+					   if(ct > maxSteps) // jezeli daleko i duza predkosc przez amplitude white noise
+							run = 0;		
