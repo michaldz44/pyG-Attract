9f9ae82d53d7b98ff753af31716244a2f3221589
maciek <maciej.kaminski@pwr.edu.pl>
2017-05-09 13:32:13 +0200

    Add bitmap from problem size

--- gattract.py
+++ gattract.py
@@ -1,6 +1,8 @@
 import argparse
 import json
 import sys
+from golem import Golem
+
 def main():
     parser = argparse.ArgumentParser(description='Gravitational attractors')
     parser.add_argument('--dt', type=float, default=0.05, help='timestep')
@@ -8,9 +10,11 @@ def main():
     parser.add_argument('--pot_d', type=float, default=0.005, help='potential diameter')
     parser.add_argument('--term_v', type=float, default=0.005, help='terminatig velocity')
     parser.add_argument('--mu', type=float, default=0.7, help='friction coefficient')
-    
+    parser.add_argument('--size', type=int, default=100, help='Problem size (N as it will compute NxN matrix) ')
     args = parser.parse_args()
-    print(args)
+
+    bitmap=[[Golem(x,y,args) for x in range(N)] for y in range(N)]
+
 
 if __name__ == "__main__":
     main()
