dd4459b801d3d6fff14db725483f0353bb76424b
maciek <maciej.kaminski@pwr.edu.pl>
2017-05-10 06:10:21 +0200

    Initial Attractor functionality

--- attractors.py
+++ attractors.py
@@ -1,9 +1,30 @@
 class Attractors(object):
     def __init__(self,js):
-        pass
+        assert js["type"]=="FeatureCollection"
+        assert "features" in js
+
+        self.attractors_positions=[]
+
+
+        for feature in js["features"]:
+            assert "geometry" in feature
+            assert "properties" in feature
+            assert "MASS" in feature["properties"]
+            assert feture["geometry"]["type"]=="Point"
+
+            self.attractors_positions.append({
+                "mass":feature["properties"]["MASS"],
+                "color":feature["properties"]["COLOR"],
+                "position":complex(*feature["geometry"]["coordinates"])
+            })
+
+    #def distances_list(self,position):
+    #    return
 
     def get_force(self,position):
-        pass
+        return sum([attractor["mass"]/abs(position-attractor["position"])**(2) for attractor in  self.attractors])
 
     def min_distance(self,position):
-        pass
+        return min([abs(position-attractor["position"]) for attractor in self.attractors])
+
+    def
